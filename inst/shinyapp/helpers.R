24fdd58e438960e08d03dc2c3cc61f2001534498
lozalojo <lozalojo@gmail.com>
2017-06-06 07:45:57 +0200

    Fix

--- inst/shinyapp/helpers.R
+++ inst/shinyapp/helpers.R
@@ -1541,10 +1541,10 @@ zip.present<-function() file.exists(Sys.getenv("R_ZIPCMD"))
 #' @param wait The amount of time to wait before showing the busy indicator. The
 #'   default is 1000 which is 1 second.
 #'
-busyIndicator <- function(text = "Calculation in progress..",img = "shinyapp/www/ajaxloaderq.gif", wait=1000) {
+busyIndicator <- function(text = "Calculation in progress..",img = "ajaxloaderq.gif", wait=1000) {
   tagList(
     singleton(tags$head(
-      tags$link(rel="stylesheet", type="text/css",href="shinyapp/www/busyIndicator.css")
+      tags$link(rel="stylesheet", type="text/css",href="busyIndicator.css")
     ))
     ,div(class="shinysky-busy-indicator",p(text),img(src=img))
     ,tags$script(sprintf(
