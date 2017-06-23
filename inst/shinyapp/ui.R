b485f667a62219e3967fbc631f8609e283ce24c5
Jose Eugenio Lozano Alonso <LozAloJo@jcyl.red>
2017-06-23 14:37:26 +0200

    Updated version number for CRAN submission

--- inst/shinyapp/ui.R
+++ inst/shinyapp/ui.R
@@ -24,7 +24,7 @@ shinyUI(dashboardPage(skin = "black",
                       # Tricky way of placing elements in dashboardHeader, expects a tag element of type li and class dropdown, 
                       # so we can pass such elements instead of dropdownMenus
                       dashboardHeader(title = "MEM dashboard",
-                                      tags$li("memapp v2.1.20170615, code under GPLv2 at",
+                                      tags$li("memapp v2.2.20170623, code under GPLv2 at",
                                               class = "dropdown"),
                                       tags$li(a(href = 'https://github.com/lozalojo/memapp',
                                                 img(src = 'GitHub_Logo.png',
