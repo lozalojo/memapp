65734b87fe0d071e3c4d073140104238ca4ce9df
Jose Eugenio Lozano Alonso <LozAloJo@jcyl.red>
2017-10-18 14:49:54 +0200

    Fixed error in imports when building the package

--- R/get.languages.R
+++ R/get.languages.R
@@ -6,7 +6,7 @@ get.languages<-function(){
   translation.dir<-utils::head(translation.loc[dir.exists(translation.loc)],1)
   langfiles<-data.frame(filename=tools::file_path_sans_ext(list.files(translation.dir, ".*\\.txt")), stringsAsFactors = F)
   langfiles$lcidstring<-tolower(langfiles$filename)
-  locales<-read.delim(paste0(translation.dir,"/locales.txt"),header=T,sep=";",row.names=NULL,fill=T,colClasses="character", as.is=T)
+  locales<-utils::read.delim(paste0(translation.dir,"/locales.txt"),header=T,sep=";",row.names=NULL,fill=T,colClasses="character", as.is=T)
   languages<-merge(locales,langfiles,by="lcidstring")
   languages
 }
