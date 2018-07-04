ec8aea7f94fb451d3796585ecb5ec57ff461f45e
Jose Eugenio Lozano Alonso <LozAloJo@jcyl.red>
2018-07-04 13:02:17 +0200

    Added right sidebar with shinydashboardPlus

--- inst/shinyapp/ui.R
+++ inst/shinyapp/ui.R
@@ -1,6 +1,7 @@
 library("shiny")
 library("shinythemes")
 library("shinydashboard")
+library("shinydashboardPlus")
 library("shinyBS")
 library("shinyjs")
 library("RColorBrewer")
@@ -31,99 +32,102 @@ names(languages.list)<-languages$lang_name
 running.versions<-get.r.versions()
 
 shinyUI(
-  dashboardPage(skin = "black",
-                ###################################
-                ### HEADER SECTION              ###
-                ###################################
-                # Tricky way of placing elements in dashboardHeader, expects a tag element of type li and class dropdown, 
-                # so we can pass such elements instead of dropdownMenus
-                dashboardHeader(title = "MEM dashboard",
-                                tags$li(paste(running.versions$r,"/",running.versions$platform,", memapp ",running.versions$memapp,", mem ",running.versions$mem," - code under GPLv2 at",sep=""),
-                                        class = "dropdown"),
-                                tags$li(a(href = 'https://github.com/lozalojo/',
-                                          target="_blank",
-                                          img(src = 'GitHub_Logo.png',
-                                              title = "José E. Lozano", height = "30px"),
-                                          style = "padding-top:10px; padding-bottom:0px;"),
-                                        class = "dropdown"),
-                                tags$li(a(href = 'http://www.icscyl.com',
-                                          target="_blank",
-                                          img(src = 'logoiecscyl.gif',
-                                              title = "IECSCyL", height = "40px"),
-                                          style = "padding-top:5px; padding-bottom:0px;"),
-                                        class = "dropdown"),
-                                tags$li(a(onclick = "setTimeout(function(){window.close();}, 100); ",
-                                          icon("power-off", "fa-2x"),
-                                          title = "Power off"),
-                                        class = "dropdown")),
-                ###################################
-                ### LEFT PANEL SECTION          ###
-                ###################################
-                dashboardSidebar(width='250px', 
-                                 ################################
-                                 ###    Load data          ######
-                                 ################################
-                                 uiOutput("uifile"),
-                                 uiOutput("uiDataset"),
-                                 ################################
-                                 ###    Model                ####
-                                 ################################
-                                 uiOutput("uiModel"),
-                                 ################################
-                                 ###    Surveillance         ####
-                                 ################################
-                                 uiOutput("uiSurveillance"),
-                                 ################################
-                                 ###    Visualize            ####
-                                 ################################
-                                 uiOutput("uiVisualize"),
-                                 ################################
-                                 ###  Thresholds             ####
-                                 ################################
-                                 uiOutput("uiThresholds")
-                ),
-                
-                ###################################
-                ### BODY/MAIN SECTION           ###
-                ###################################
-                
-                dashboardBody(
-                  # All credits of the busy-indicator goes to AnalytixWare/ShinySky, I was forced to include the code
-                  # here because the package does not have a CRAN release.
-                  tags$body(inlineCSS(list(".shinysky-busy-indicator" = "position: absolute !important; z-index:800; "))),
-                  tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
-                  uiOutput("uiTitle"),
-                  fluidPage(
-                    fluidRow(
+  dashboardPagePlus(title = "The Moving Epidemic Method Web Application",
+                    skin = "black",
+                    collapse_sidebar = FALSE,
+                    ###################################
+                    ### HEADER SECTION              ###
+                    ###################################
+                    # Tricky way of placing elements in dashboardHeader, expects a tag element of type li and class dropdown, 
+                    # so we can pass such elements instead of dropdownMenus
+                    header = dashboardHeaderPlus(title = "MEM dashboard",
+                                                 enable_rightsidebar = TRUE,
+                                                 rightSidebarIcon = "gears",
+                                                 tags$li(paste(running.versions$r,"/",running.versions$platform,", memapp ",running.versions$memapp,", mem ",running.versions$mem," - code under GPLv2 at",sep=""),
+                                                         class = "dropdown"),
+                                                 tags$li(a(href = 'https://github.com/lozalojo/',
+                                                           target="_blank",
+                                                           img(src = 'GitHub_Logo.png',
+                                                               title = "José E. Lozano", height = "30px"),
+                                                           style = "padding-top:10px; padding-bottom:0px;"),
+                                                         class = "dropdown"),
+                                                 tags$li(a(href = 'http://www.icscyl.com',
+                                                           target="_blank",
+                                                           img(src = 'logoiecscyl.gif',
+                                                               title = "IECSCyL", height = "40px"),
+                                                           style = "padding-top:5px; padding-bottom:0px;"),
+                                                         class = "dropdown"),
+                                                 tags$li(a(onclick = "setTimeout(function(){window.close();}, 100); ",
+                                                           icon("power-off", "fa-2x"),
+                                                           title = "Power off"),
+                                                         class = "dropdown")),
+                    ###################################
+                    ### LEFT PANEL SECTION          ###
+                    ###################################
+                    sidebar = dashboardSidebar(width='250px', 
+                                               ################################
+                                               ###    Load data          ######
+                                               ################################
+                                               uiOutput("uifile"),
+                                               uiOutput("uiDataset"),
+                                               ################################
+                                               ###    Model                ####
+                                               ################################
+                                               uiOutput("uiModel"),
+                                               ################################
+                                               ###    Surveillance         ####
+                                               ################################
+                                               uiOutput("uiSurveillance"),
+                                               ################################
+                                               ###    Visualize            ####
+                                               ################################
+                                               uiOutput("uiVisualize"),
+                                               ################################
+                                               ###  Thresholds             ####
+                                               ################################
+                                               uiOutput("uiThresholds")
+                    ),
+                    ###################################
+                    ### BODY/MAIN SECTION           ###
+                    ###################################
+                    body = dashboardBody(
+                      # All credits of the busy-indicator goes to AnalytixWare/ShinySky, I was forced to include the code
+                      # here because the package does not have a CRAN release.
+                      tags$body(inlineCSS(list(".shinysky-busy-indicator" = "position: absolute !important; z-index:800; "))),
+                      tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
+                      uiOutput("uiTitle"),
                       ###################################
                       ### BODY/MAIN SECTION           ###
                       ###   FIRST COLUMN DEFINITION   ###
                       ###################################
-                      column(9, offset = 0, style='padding:0px;',
-                             #######################################
-                             ### BODY/MAIN SECTION               ###
-                             ###   FIRST COLUMN DEFINITION       ###
-                             ###      FIRST PART: OUTPUTS        ###
-                             #######################################
-                             uiOutput("uiProcedures")
-                      ),
-                      ###################################
-                      ### BODY/MAIN SECTION           ###
-                      ###   SECOND COLUMN DEFINITION  ###
-                      ###################################
-                      column(3, offset = 0, style='padding:0px;',
-                             uiOutput("uiTextoptions"),
-                             uiOutput("uiGraphoptions"),
-                             uiOutput("uiMEMoptions"),
-                             uiOutput("uiSupport"),
-                             shinydashboard::box(
-                               title="", solidHeader = TRUE, status = "warning", width = 12,
-                               uiOutput("uiLanguage"),
-                               selectInput("language", label = "", choices = languages.list, size=1, selectize = FALSE, selected = "en_GB")
-                             )
+                      #######################################
+                      ### BODY/MAIN SECTION               ###
+                      ###   FIRST COLUMN DEFINITION       ###
+                      ###      FIRST PART: OUTPUTS        ###
+                      #######################################
+                      uiOutput("uiProcedures")
+                    ),
+                    ###################################
+                    ### LEFT PANEL SECTION          ###
+                    ###################################
+                    rightsidebar = rightSidebar(
+                      width=350,
+                      background = "light",
+                      rightSidebarTabContent(
+                        id = 1,
+                        title = NULL,
+                        icon = NULL,
+                        active = TRUE,
+                        uiOutput("uiTextoptions"),
+                        uiOutput("uiGraphoptions"),
+                        uiOutput("uiMEMoptions"),
+                        uiOutput("uiSupport"),
+                        shinydashboard::box(
+                          title="", solidHeader = TRUE, status = "warning", width = 12,
+                          uiOutput("uiLanguage"),
+                          selectInput("language", label = "", choices = languages.list, size=1, selectize = FALSE, selected = "en_GB")
+                        )
                       )
-                    )
-                  )
-                )
+                    )                
   )
 )
