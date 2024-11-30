65f95b6310857043d5a2c3a7baebd4ffccb7adcd
José Lozano <lozanoje@gmail.com>
2024-11-30 09:47:05 +0100

    Resizing all plots, first working version

--- inst/shinyapp/ui.R
+++ inst/shinyapp/ui.R
@@ -106,6 +106,7 @@ shinyUI(
       # All credits of the busy-indicator goes to AnalytixWare/ShinySky,
       # I was forced to include the code here because the package does
       # not have a CRAN release.
+      tags$head(tags$script(src = "resize.js")),
       tags$body(inlineCSS(list(".shinysky-busy-indicator" = "position: absolute !important; z-index:800; "))),
       tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
       uiOutput("uiTitle"),
